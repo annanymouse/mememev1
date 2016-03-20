5cbe280a960720d523ae8ee91037672789c46c6d
Anna Hull <annachull@gmail.com>
2016-03-20 02:28:29 -0700

    Initial Commit

---
+++ PhotoPicker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhotoPicker
+//
+//  Created by Anna Hull on 3/20/16.
+//  Copyright © 2016 Anna Hull. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
